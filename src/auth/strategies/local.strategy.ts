f5a2df976299db960f63913dc30d4f56799a18f4
naihe138 <370215230@qq.com>
2021-11-28 22:12:58 +0800

    delete test file

--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -11,6 +11,8 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
 
   async validate(username: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(username, password);
+    console.log(123);
+
     if (!user) {
       throw new UnauthorizedException();
     }
