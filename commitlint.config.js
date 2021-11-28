d8f3e2f9f43bac396f99711bc762d9b7cbe1a978
naihe138 <370215230@qq.com>
2021-11-28 22:34:39 +0800

    husky5

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,27 +1,3 @@
 module.exports = {
-  parserPreset: {
-    parserOpts: {
-      headerPattern: /^(\w*)(?:\((.*)\))?:[ ]?(.*)$/,
-      headerCorrespondence: ['type', 'scope', 'subject'],
-    },
-  },
-  rules: {
-    'type-empty': [2, 'never'],
-    'type-case': [2, 'always', 'lower-case'],
-    'subject-empty': [2, 'never'],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat', // 特性增加
-        'fix', // 功能修复
-        'docs', // 文档维护
-        'style', // 样式维护
-        'refactor', // 重构功能
-        'test', // 增加测试用例
-        'chore', // 其他事务
-        'wip', // 待完成功能
-      ],
-    ],
-  },
+  extends: ['@commitlint/config-conventional'],
 };
